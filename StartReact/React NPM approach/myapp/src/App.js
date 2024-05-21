4ad5c91ea4aec6408749c02b446dc3fa61f92101
sachin <sharmasachinwins@gmail.com>
2024-05-21 07:47:48 +0530

    setting states

--- StartReact/React NPM approach/myapp/src/App.js
+++ StartReact/React NPM approach/myapp/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // import PropTypes from "prop-types";
 
 // class App extends React.Component {
@@ -24,15 +24,51 @@ import React from "react";
 // you to pass components or elements as data to other components, which can then render those children as part of their output
 
 
-const App = (props) => {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <h2>This is a div</h2>
-      <p>{props.children}</p> 
-    </div>
-  );
-};
+// const App = (props) => {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <h2>This is a div</h2>
+//       <p>{props.children}</p> 
+//     </div>
+//   );
+// };
 
+// export default App;
+
+
+class App extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            fname: "Sachin",
+            lname: "Shamra"
+        }
+
+    }
+//passing object
+    handleclick = () => {
+        this.setState({
+            fname:"David",
+            lname: "Milan"
+        })
+
+    //set state passinf function
+    // handleclick = () => {
+    //     this.setState(function(state,props){
+    //         console.log(state);
+    //     })
+        
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello   </h1>
+                <h2>My name is {this.state.fname} {this.state.lname}</h2>
+                    <button onClick={this.handleclick}>Update </button>
+            </div>
+        );
+    }
+}
 export default App;
-
--- StartReact/React NPM approach/myapp/src/ReactEvent/Event.js
+++ StartReact/React NPM approach/myapp/src/ReactEvent/Event.js
@@ -5,17 +5,19 @@ const Eventlist = ()=> {
     const purple = "#70a4e9;"
     const [bg, setbg] = useState(purple)
     const [ename, setename] = useState("Click Me ")
-    const bgchnage = ()=>{
+    const bgchnage = (value)=>{
         let newbg = "rgb(255, 191, 201)"
+
+        console.log(value);
         setbg(newbg)
-        setename("Aaye haye Aaye haye Bado Badi Bado Badi:SM ")
+        setename("Aaye haye Aaye haye Bado Badi Bado Badi ")
 
     }
 
 return(
     <>
     <div  className= "main"style={{backgroundColor: bg}}>
-        <button  className= "btn"onClick={bgchnage}>{ename}</button>
+        <button  className= "btn"onClick={()=>bgchnage("Sachin")}>{ename}</button>
 
     </div>
     </>
