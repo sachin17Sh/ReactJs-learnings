ba7c2d7259c0bf2f5477dd4e36491a8068a7ae89
sachin <sharmasachinwins@gmail.com>
2024-05-24 07:26:04 +0530

    adding css to stopwatch

--- StartReact/React NPM approach/myapp/src/Stopwatch/stopwatch.js
+++ StartReact/React NPM approach/myapp/src/Stopwatch/stopwatch.js
@@ -8,21 +8,46 @@ export const Stw = () => {
         let timer;
         if (start) {
             timer = setInterval(() => {
-                settime(prevTime => prevTime + 1);
-            }, 1000);
-        } else {
-            clearInterval(timer);
-        }
-        return () => clearInterval(timer); 
-    }, [start]);  //dependency array 
+                settime(prevTime => prevTime + 10);
+            },10);
+        } 
+        return () => clearInterval(timer);
+    },
     
+     [start]); 
+    //dependency array 
+    function handleStart() {
+        setstart(true)
 
+    }
+    function handlePause() {
+        setstart(false)
+    }
+    function handleReset() {
+        setstart (false)
+        settime(0)
+    }
+    function formatTime(){
+      let hours = Math.floor(time/(1000*60*60))
+      let minutes = Math.floor(time/(1000*60)%60)
+      let seconds = Math.floor(time/(1000)%60)
+      let milliseconds = Math.floor((time%1000)/10)
+
+   hours = String(hours).padStart(2, "0");
+   minutes = String(minutes).padStart(2, "0");
+   seconds = String(seconds).padStart(2, "0");
+   milliseconds = String(milliseconds).padStart(2, "0");
+
+      return `${hours}:${minutes}:${seconds}:${milliseconds}`
+    }
+    
     return (
-     <div className='main'>
-        <h1>Stopwatch: {time} seconds</h1>
-        <button className="btn" onClick={handleStart}>Start</button>
-        <button className="btn" onClick={handlePause}>Pause</button>
-        <button className="btn" onClick={handleReset}>Reset</button>
-    </div>
+        <div className='mainwtc'>
+            <h1 className='displaywatch'> {formatTime()} </h1>
+         <div className='controls'>   <button className="btnstr" onClick={handleStart}>Start</button>
+            <button className="btnstr" onClick={handlePause} >Pause</button>
+            <button className="btnstr" onClick={handleReset}>Reset</button>
+            </div>
+        </div>
     )
 }
